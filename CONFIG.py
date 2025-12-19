495cab0da416fcd785d5b4aa76eedb2bab54e7b0
cassandre <github@cassandre.klo.re>
2025-12-19 21:58:17 +0100

    storage

--- CONFIG.py
+++ CONFIG.py
@@ -2,10 +2,10 @@
 #IAF_root = "Data/IAF_TestSet"
 IAF_root = "Benchmarks/ICCMA2023-inc"
 
-#sem = "ST"
-sem = "PR"
+sem = "ST"
+#sem = "PR"
 #sem = "GR"
 
-completion = "MINMAX"
+#completion = "MINMAX"
 #completion = "MIN"
-#completion = "MAX"
+completion = "MAX"
